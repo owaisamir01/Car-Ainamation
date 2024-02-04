d622cb711362c1ab93b0e3be9089eca2fbdf0bd2
Owais <sheikhowais0001@gmail.com>
2024-02-04 18:26:22 +0500

    Project Uploaded

---
+++ script.js
@@ -0,0 +1,14 @@
+// var audio=document.createElement('audio');
+// audio.setAttribute('src','sound.mp3');
+// audio.loop=true;
+// audio.play();
+
+
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3'); 
+audio.loop=true;
+audio.autoplay;
+
+let audioElement=new Audio('sound.mp3');
+audioElement.play();
+
